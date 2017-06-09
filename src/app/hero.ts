b1b88ff67be876be276cbfd22a26d4c7f84b47be
niklas-schmidt@outlook.de <aCe//.gg>
2017-06-09 12:13:50 +0200

    added hero-form component

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,3 +1,12 @@
 /**
  * Created by n.schmidt on 09.06.2017.
  */
+export class Hero {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {}
+}
